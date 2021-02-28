e75716baa7894bb0334c4cd96fc53054576285eb
Tim Buckland <tim.buckland@gmail.com>
2021-02-28 22:33:08 +0000

    use env, util and custom command

--- cypress/integration/canary/canary.js
+++ cypress/integration/canary/canary.js
@@ -1,7 +1,10 @@
 /// <reference types="cypress" />
+const util = require('../../helper/util');
 describe('Frontend Canary', () => {
     it('checks the frontend canary page', () => {
         cy.log(`Running on brand ${Cypress.env('brandHost')}`);
         expect(true).to.equal(true);
+        cy.log(`German url is ${util.germanBaseUrl()}`);
+        cy.customCommand();
     });
 });
